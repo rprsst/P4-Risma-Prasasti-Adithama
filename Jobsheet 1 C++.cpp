ab5a74c19b5eb2122f812c3cb5cea8ab1c5a915e
rprsst <65282198+rprsst@users.noreply.github.com>
2020-05-14 05:10:13 +0800

    Create Jobsheet 1 C++.cpp
    Menampilkan array satu dimensi.

---
+++ Jobsheet 1 C++.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Nama: Risma Prasasti Adithama" << endl;
+	cout << "NIM : F1B019124" << endl;
+	cout << "Kelompok: 25" << endl;
+	cout << endl;
+	
+	string NIM[5] = {"F1B019123", "F1B019124", "F1B019125", "F1B019127", "F1B019146"};
+	
+	cout << NIM[0] << " : Rio Syarifuddin" << endl;
+	cout << NIM[1] << " : Risma Prasasti Adithama" << endl;
+	cout << NIM[2] << " : Riyan Fathurrahman" << endl;
+	cout << NIM[3] << " : Rohul Iman" << endl;
+	cout << NIM[4] << " : Zahratul laily" << endl;
+		
+	return 0;
+}
