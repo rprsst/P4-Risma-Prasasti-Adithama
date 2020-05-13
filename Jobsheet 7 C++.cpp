b95385181e8ee6ef2bc285c475b5d01ca57efb1f
rprsst <65282198+rprsst@users.noreply.github.com>
2020-05-14 05:21:17 +0800

    Create Jobsheet 7 C++.cpp
    Membuat program mengisi nilai array dengan perulangan dengan index array dan batas pengulangan menggunakan NIM terakhir setiap mahasiswa.

---
+++ Jobsheet 7 C++.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Nama: Risma Prasasti Adithama" << endl;
+	cout << "NIM : F1B019124" << endl;
+	cout << "Kelompok: 25" << endl;
+	cout << endl;
+
+	int x[5], y;
+	for (y = 0; y < 5; y++)
+	{
+		cout << "Nilai index " << y << " = ";
+		cin >> x[y]; 
+	}
+	
+	return 0;
+}
